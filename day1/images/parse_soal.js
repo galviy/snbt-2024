9fc9786409304b805d2560aba2fd65df151b9c9b
Galvin Shalahudin <68190778+galviy@users.noreply.github.com>
2024-09-16 10:44:50 +0700

    Add files via upload

--- day1/images/parse_soal.js
+++ day1/images/parse_soal.js
@@ -16,10 +16,10 @@ function readfile(){
 
         //handling soal cerita bergambar
 
-        console.log(`${i+1}.`)
+        console.log(`### ${i+1}.\n\n`)
         let pukimak = data.soal[i].soalCerita.imageUrls;
         if(data.soal[i].soalCerita.text.length != 0){
-            console.log(`${data.soal[i].soalCerita.text}`)
+            console.log(`**Question:** ${data.soal[i].soalCerita.text}`)
         } 
         
 
@@ -29,16 +29,16 @@ function readfile(){
                 let data_soal = data.soal[i].soalCerita.imageUrls[j];
                 if(apakahadacuk(data_soal.replace("{{root_media}}", ""))){
                     //console.log()
-                    console.log(`Soal cerita Bergambar -> ${data_soal.replace("{{root_media}}", "")} `)
+                    console.log(`![](images/${data_soal.replace("{{root_media}}", "")})\n\n`)
                 } else {
-                    console.log("jancuk g ada anjg")
+                    console.log("File terhapus dari database")
                 }
             }
         }
             
         
         if(data.soal[i].soal.text.length != 0){
-            console.log(`${data.soal[i].soal.text}`)  
+            console.log(`**Soal:** ${data.soal[i].soal.text}\n`)  
         }
         
 
@@ -47,9 +47,9 @@ function readfile(){
        let soal_gambar = data.soal[i].soal.imageUrls;
        for(let j = 0; j < soal_gambar.length; j++){
         if(apakahadacuk(soal_gambar[j].replace("{{root_media}}", ""))){
-            console.log(`Soal cerita Bergambar -> ${soal_gambar[j].replace("{{root_media}}", "")} `)
+            console.log(`![](images/${soal_gambar[j].replace("{{root_media}}", "")})\n\n`)
         } else {
-            console.log("jancuk g ada anjg")
+            console.log("File terhapus dari database")
         }
        }
         
@@ -57,15 +57,16 @@ function readfile(){
        //handling opsi soal
        let opsi_soal = data.soal[i].opsiSoal;
        if(data.soal[i].opsiSoal == null){
-        console.log("ini Soal essay")
+        console.log(`### Soal Essay`)
        } else {
-        console.log("Opsi Soal")
+        console.log("**Opsi:**\n\n")
         for(let l = 0; l < 5; l++){
             if(opsi_soal[l] == null){
                 break;
             }
-            console.log(`${huruf[l]}. ${opsi_soal[l].text}`)
+            console.log(`- ${huruf[l]}. ${opsi_soal[l].text}`)
         }
+		console.log(`\n`)
         for(let j = 0; j < 4; j++){
             if(opsi_soal[j] == null){
                 break;
@@ -76,7 +77,7 @@ function readfile(){
           for(let k = 0; k < opsi_soal_gambar.length; k++){
             if(apakahadacuk(opsi_soal_gambar[k].replace("{{root_media}}", ""))){
 
-                console.log(`Gambar ${opsi_soal_gambar[k].replace("{{root_media}}", "")} ada didalam `)
+                console.log(`![](images/${opsi_soal_gambar[k].replace("{{root_media}}", "")})\n`)
             } else {
                 console.log("jancuk g ada anjg")
             }
